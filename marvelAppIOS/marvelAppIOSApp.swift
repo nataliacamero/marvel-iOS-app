9beca1601d43372b5a24d976baa38d12e81d98dc
Natalia Camero Carreño <nataliacameroc@gmail.com>
2023-11-19 21:50:12 +0100

    git add various

--- marvelAppIOS/marvelAppIOSApp.swift
+++ marvelAppIOS/marvelAppIOSApp.swift
@@ -10,10 +10,15 @@ import SwiftUI
 @main
 struct marvelAppIOSApp: App {
     let persistenceController = PersistenceController.shared //Core data
+
+    //ViewMOdel global
+        @StateObject var rootViewModel = RootViewModel()
+    
     var body: some Scene {
         WindowGroup {
             CharactersListView(characters: charactersArray)
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(rootViewModel)
         }
     }
 }
