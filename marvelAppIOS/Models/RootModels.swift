88cd50f39cadcb259ba922c1f6a5dfc7e30eb7c3
Natalia Camero Carreño <nataliacameroc@gmail.com>
2023-11-21 21:51:52 +0100

    add mejora del modelo

--- marvelAppIOS/Models/RootModels.swift
+++ marvelAppIOS/Models/RootModels.swift
@@ -8,49 +8,31 @@
 import Foundation
 
 
-import Foundation
-
-// MARK: - WelcomeElement
-struct WelcomeElement: Codable {
+// MARK: - ModelMarvel -
+struct ModelMarvel: Codable {
     let code: Int
     let status, copyright, attributionText, attributionHTML: String
     let etag: String
-    let data: DataClass
+    let data: CharactersData
 }
 
-// MARK: - DataClass
-struct DataClass: Codable {
+// MARK: - CharactersData -
+struct CharactersData: Codable {
     let offset, limit, total, count: Int
     let results: [CharacterList]
 }
 
-// MARK: - Character
-struct CharacterListr: Codable, Identifiable {
-    let id: Int?
-    let name, description: String?
-    let modified: String?
-    //let thumbnail: Thumbnail?
-    let resourceURI: String?
-    //let series: Series?
-    //let stories: Stories?
-    //let events: Series?
-    //let urls: [URLElement]?
-}
-
+// MARK: - CharacterList -
 struct CharacterList: Codable, Identifiable {
     let id: Int
-    let name: String
+    let name: String?
     let description: String?
-    let modified: Date
     let thumbnail: Thumbnail
-    let resourceURI: String
-    let series: Series
-    let stories: Stories
-    let events: Series
-    let urls: [URLElement]
+    let resourceURI: String?
+    //let series: Series
 }
 
-// MARK: - Series
+// MARK: - Series -
 struct Series: Codable {
     let available: Int?
     let collectionURI: String?
@@ -58,49 +40,13 @@ struct Series: Codable {
     let returned: Int?
 }
 
-// MARK: - ComicsItem
+// MARK: - ComicsItem -
 struct SeriesItem: Codable {
     let resourceURI: String
     let name: String
 }
 
-// MARK: - Stories
-struct Stories: Codable {
-    let available: Int
-    let collectionURI: String
-    let items: [StoriesItem]
-    let returned: Int
-}
-
-// MARK: - StoriesItem
-struct StoriesItem: Codable {
-    let resourceURI: String
-    let name: String
-    let type: ItemType
-}
-
-enum ItemType: String, Codable {
-    case cover = "cover"
-    case empty = ""
-    case interiorStory = "interiorStory"
-}
-
-// MARK: - Thumbnail
-struct Thumbnails: Codable {
-    let path: String
-    let thumbnailExtension: Extension
-
-    enum CodingKeys: String, CodingKey {
-        case path
-        case thumbnailExtension = "extension"
-    }
-}
-
-enum Extensions: String, Codable {
-    case gif = "gif"
-    case jpg = "jpg"
-}
-
+// MARK: - Thumbnail -
 struct Thumbnail: Codable {
     let path: String
     let thumbnailExtension: Extension
@@ -111,37 +57,9 @@ struct Thumbnail: Codable {
     }
 }
 
-// Enum para el tipo de extensión
+// MARK: - Enum para el tipo de extensión -
 enum Extension: String, Codable {
+    case gif = "gif"
     case jpg = "jpg"
     case png = "png"
-    // Agrega otras extensiones si es necesario
 }
-
-// MARK: - URLElement
-struct URLElements: Codable {
-    let type: URLType
-    let url: String
-}
-
-enum URLTypes: String, Codable {
-    case comiclink = "comiclink"
-    case detail = "detail"
-    case wiki = "wiki"
-}
-
-// Define la estructura URLElement con un enum para el tipo de URL
-struct URLElement: Codable, Identifiable {
-    let id = UUID() // Necesario para Identifiable
-    let type: URLType
-    let url: String
-}
-
-// Enum para el tipo de URL
-enum URLType: String, Codable {
-    case detail
-    case wiki
-    case comiclink
-}
-
-typealias Welcome = [WelcomeElement]
