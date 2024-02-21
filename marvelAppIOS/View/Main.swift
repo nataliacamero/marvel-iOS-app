6815d4b5ac9d79c99f85492ccd51d443f306f657
Natalia Camero Carreño <nataliacameroc@gmail.com>
2024-02-21 12:26:09 +0100

    ✨ [main] created basic main view

--- marvelAppIOS/View/Main.swift
+++ marvelAppIOS/View/Main.swift
@@ -5,4 +5,26 @@
 //  Created by Natalia Camero on 21/2/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MainView: View {
+    //Enviroment variable that is injected into the delegate
+    @EnvironmentObject var viewModel: RootViewModel
+    var body: some View {
+        switch viewModel.status {
+        case Status.none:
+            Text("None State")
+        case Status.loading:
+            Text("Loading...")
+        case Status.loaded:
+            Text("Loaded.")
+        case Status.error(error: let errorString):
+            Text("Error: \(errorString)")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+        .environmentObject(RootViewModel())
+}
